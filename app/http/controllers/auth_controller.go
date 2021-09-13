6865e95ce21a6ba770374a43d406125109c1fa2c
Enzo <admin@tcp.so>
2021-09-14 06:20:03 +0800

    修复session没成功保存的bug 完善验证码配置文件相关

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -8,6 +8,7 @@ import (
 	"oneQrCode/app/models/user"
 	"oneQrCode/app/requests"
 	"oneQrCode/pkg/captcha"
+	"oneQrCode/pkg/config"
 	"oneQrCode/pkg/logger"
 )
 
@@ -38,90 +39,16 @@ func (ac *AuthController) DoRegister(c *gin.Context) {
 
 // GetCaptcha 获取验证码.
 func (ac *AuthController) GetCaptcha(c *gin.Context) {
-	config := `{
-				  "ShowLineOptions": [
-					"2",
-					"4",
-					"8"
-				  ],
-				  "CaptchaType": "string",
-				  "Id": "",
-				  "VerifyValue": "duku3m",
-				  "DriverAudio": {
-					"Length": 6,
-					"Language": "zh"
-				  },
-				  "DriverString": {
-					"Height": 60,
-					"Width": 230,
-					"ShowLineOptions": 14,
-					"NoiseCount": 480,
-					"Source": "234568wertyupkjhfdsazxcvnm",
-					"Length": 6,
-					"Fonts": [
-					  "Flim-Flam.ttf",
-					  "DeborahFancyDress.ttf",
-					  "actionj.ttf",
-					  "RitaSmith.ttf",
-					  "chromohv.ttf",
-					  "ApothecaryFont.ttf"
-					],
-					"BgColor": {
-					  "R": 0,
-					  "G": 0,
-					  "B": 0,
-					  "A": 0
-					}
-				  },
-				  "DriverMath": {
-					"Height": 60,
-					"Width": 240,
-					"ShowLineOptions": 14,
-					"NoiseCount": 0,
-					"Length": 6,
-					"Fonts": [
-					  "wqy-microhei.ttc"
-					],
-					"BgColor": {
-					  "R": 0,
-					  "G": 0,
-					  "B": 0,
-					  "A": 0
-					}
-				  },
-				  "DriverChinese": {
-					"Height": 60,
-					"Width": 320,
-					"ShowLineOptions": 0,
-					"NoiseCount": 0,
-					"Source": "设想,你在,处理,消费者,的音,频输,出音,频可,能无,论什,么都,没有,任何,输出,或者,它可,能是,单声道,立体声,或是,环绕立,体声的,,不想要,的值",
-					"Length": 2,
-					"Fonts": [
-					  "wqy-microhei.ttc"
-					],
-					"BgColor": {
-					  "R": 125,
-					  "G": 125,
-					  "B": 0,
-					  "A": 118
-					}
-				  },
-				  "DriverDigit": {
-					"Height": 80,
-					"Width": 240,
-					"Length": 5,
-					"MaxSkew": 0.7,
-					"DotCount": 80
-				  }
-				}`
 	session := sessions.Default(c)
-	decoder := json.NewDecoder(bytes.NewBufferString(config))
+	decoder := json.NewDecoder(bytes.NewBufferString(config.GetString("captcha.style_json")))
 	var verificationCode captcha.Captcha
 	err := decoder.Decode(&verificationCode)
 	logger.CheckError(err)
 	id, b64s, err := verificationCode.NewCaptcha()
 	logger.CheckError(err)
 	session.Set("captcha_id", id)
+	err = session.Save()
+	logger.CheckError(err)
 	c.JSON(200, gin.H{
 		"result": b64s,
 	})
